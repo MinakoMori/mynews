c6d93da59da3f84b09dc0d21c42862d951705e11
森美奈子 <moegi-48-92@docomo.ne.jp>
2021-04-28 04:08:46 +0000

    210428 13:08更新

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -12,6 +12,7 @@
         
         @section('content')
         <div class="container">
+            <h1>MyNews</h1>
             <hr color="#c0c0c0">
             @if (!is_null($headline))
             <div class="row">
