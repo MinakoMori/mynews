18e9a43be9a60ec1906b077f7baad6719adbde75
森美奈子 <moegi-48-92@docomo.ne.jp>
2021-04-28 04:47:17 +0000

    210428 13:47

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -29,7 +29,7 @@
                     </div>
                     <div class="col-md-6">
                         <div class="title p-2">
-                            <h1>{{ str_limit($headline->title, 70) }}</h1>
+                            <h2>{{ str_limit($headline->title, 70) }}</h2>
                         </div>
                         <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
                     </div>
