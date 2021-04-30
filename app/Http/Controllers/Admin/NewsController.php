3ea07d50beff3ac4e7aa8a69ed2eb5ae64cb51a9
森美奈子 <moegi-48-92@docomo.ne.jp>
2021-04-30 10:28:16 +0000

    210430 19:28

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -60,7 +60,7 @@ class NewsController extends Controller
         if (empty($news)) {
             abort(404);
         }
-        return view('admin.news', ['news_form' => $news]);
+        return view('admin.news.edit', ['news_form' => $news]);
     }
     
     public function update(Request $request)
